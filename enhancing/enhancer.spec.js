abe20ae1ed2a6089d6a9d6fa96168129a0e13e2c
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-24 16:28:47 -0500

    passing first test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+it("works", () => {
+    expect(3).toBeGreaterThan(2)
+})
+
+describe("repair function", ()=> {
+    
+    let item = {durability: 20}
+    const expected = {durability: 100}
+    expect(enhancer.repair(item)).toEqual(expected)
+})
