0c3f0bd105c76a6819fef8ad6c2cde923e469e21
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-24 17:24:34 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,7 +13,18 @@ function success(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability -= 5
+    return { ...item }
+  } else if (item.enhancement === 15) {
+    item.durability -= 10
+    return { ...item }
+  } else if (item.enhancement > 16) {
+    item.enhancement -= 1
+    item.durability -=10
+    return { ...item }
+  }
+  
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -15,9 +15,37 @@ describe("repair function", () => {
 describe("success item", () => {
     
     test("item enhancement increases by 1 if enhancement level != 20", () => {
-        let item = {durability: 50, enhancement: 50}
-        const expected = {durability: 50, enhancement: 51}
+        let item = {durability: 50, enhancement: 18}
+        const expected = {durability: 50, enhancement: 19}
         expect(enhancer.success(item)).toEqual(expected)
     })
 
+    test("item enhancement REMAINS UNCHANGED if enhancement level = 20", () => {
+        let item = {durability: 50, enhancement: 20}
+        const expected = {durability: 50, enhancement: 20}
+        expect(enhancer.success(item)).toEqual(expected)
+    })
 })
+
+describe("fail item", () => {
+    test("As this item's enhancement is less than 15, the durability of the item is decreased by 5", () => {
+        let item = {durability: 50, enhancement: 9}
+        const expected = {durability: 45, enhancement: 9}
+        expect(enhancer.fail(item)).toEqual(expected)
+    })
+    test("As this item's enhancement equals 15, the durability of the item is decreased by 10 with everything else unchanged", () => {
+        let item = {durability: 50, enhancement: 15}
+        const expected = {durability: 40, enhancement: 15}
+        expect(enhancer.fail(item)).toEqual(expected)
+    })
+    test("As this item's enhancement level is greater than 16, the enhancement level decreases by 1 and durability decreases by 10", () => {
+        let item = {durability: 50, enhancement: 17}
+        const expected = {durability: 40, enhancement: 16}
+        expect(enhancer.fail(item)).toEqual(expected)
+    })
+
+
+
+})
+
+
